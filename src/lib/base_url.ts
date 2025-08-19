980c74a15eecc19770949788070e1e4bc0fb5532
Muhammad Ranju <mdranju23@gmail.com>
2025-08-20 04:36:57 +0600

    feat: update popover and base url

--- src/lib/base_url.ts
+++ src/lib/base_url.ts
@@ -1,3 +1,3 @@
 // export const BASE_URL = "https://localhost:5000";
-export const BASE_URL = import.meta.env.BASE_URL;
-export const SOCKET_BASE_URL = import.meta.env.SOCKET_BASE_URL;
+export const BASE_URL = "https://chat-app-server-8ec3.onrender.com/api"
+export const SOCKET_BASE_URL ="https://chat-app-server-8ec3.onrender.com"
